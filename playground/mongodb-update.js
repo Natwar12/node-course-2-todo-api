9b26e49086e86a02f722e8b37b7c0a7f06ea4d71
akash <akash.deep@keste.com>
2019-01-02 15:22:06 +0530

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,25 @@
+// const MongoCLient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    db.collection('MyTodos').findOneAndUpdate({
+        _id: new ObjectID('5c1ccd345de0f373c86a4de3')
+    }, {
+        $set: {
+            name: 'akash'
+         }
+}, {
+        returnOriginal:false
+    }).then((result) =>{
+        console.log(result);
+    });
+     
+
+    });
+    
+    
